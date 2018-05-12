ecb5821f8c307790ff4258eff8a8d9af630006f7
Nye Smith <nyedess@gmail.com>
2018-05-12 21:01:24 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+    var chocolateBars = ["snickers", "hundred grand", "kit kat", "skittles"];
+    function addElementToBeginningOfArray(array, element) {
+      const one = [element, ...array]
+      return one
+    }
+    function destructivelyAddElementToBeginningOfArray (array, element){
+     array = [element, ...array]
+     return array
+    }
